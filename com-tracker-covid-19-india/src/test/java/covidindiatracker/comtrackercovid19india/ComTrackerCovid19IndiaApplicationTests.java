cee3f2ecbc0116534011a3613fb6bea66b43656b
Chitransh Agarwal <ChitranshAgarwal2000@gmail.com>
2020-10-17 13:24:04 +0530

    FIX

---
+++ com-tracker-covid-19-india/src/test/java/covidindiatracker/comtrackercovid19india/ComTrackerCovid19IndiaApplicationTests.java
@@ -0,0 +1,13 @@
+package covidindiatracker.comtrackercovid19india;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComTrackerCovid19IndiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
