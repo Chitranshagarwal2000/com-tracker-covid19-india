d166394f81b6d008bf235db004808ffac1b34044
Chitransh Agarwal <ChitranshAgarwal2000@gmail.com>
2020-10-17 13:23:30 +0530

    Fix

---
+++ com-tracker-covid-19-india/src/main/java/covidindiatracker/comtrackercovid19india/ComTrackerCovid19IndiaApplication.java
@@ -0,0 +1,13 @@
+package covidindiatracker.comtrackercovid19india;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComTrackerCovid19IndiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComTrackerCovid19IndiaApplication.class, args);
+	}
+
+}
