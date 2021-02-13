79b2f6b7911543ee9697826956352946d24efef6
Chitransh Agarwal <chitranshagarwal2000@gmail.com>
2021-02-14 04:50:41 +0530

    Corrected stop script

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 echo "Trying to kill application, checking port 5000"
-echo $(lsof -i:5000)
-kill $(lsof -i:5000 -t)
+pid=`lsof -i:5000`
+if [[ -n $pid ]]; then
+  echo "$pid"
+  kill $pid
+fi
